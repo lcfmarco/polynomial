c1ed9a214f59d33db2f15db175f6df38ab914251
Marco Lam <marcolam1111@gmail.com>
2024-01-28 21:09:22 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -19,6 +19,14 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        return repr(self.p1) + " - " + repr(self.p2)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -26,14 +34,33 @@ class Mul:
         self.p2 = p2
     
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
+            if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
                  return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
+        if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2 
+    
+    def __repr__(self):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
+            numerator = "( " + repr(self.p1) + " )"
+        else:
+            numerator = repr(self.p1)
+
+        if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
+            denominator = "( " + repr(self.p2) + " )"
+        else:
+            denominator = repr(self.p2)
+
+        return numerator + " / " + denominator
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+
